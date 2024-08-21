4b99a61f09839d7b187d0cea2fdce4d7d3e4e3b6
JackMichael Ward <jward8@wisc.edu>
2024-08-20 20:45:36 -0500

    Initial

---
+++ index.js
@@ -0,0 +1,23 @@
+const {chromium} = require("playwright");
+(async () => {
+  let browser = await chromium.launch();
+
+  let page = await browser.newPage();
+  await page.setViewportSize({width: 1280, height: 1080});
+  await page.goto("https://www.weather.gov");
+  let heading = await page.getByRole('heading');
+  let headerText = await heading.textContent();
+
+  await page.goto("https://www.weather.gov/briefing");
+  await page.locator(".div-half img").first().screenshot({path: "forecast-map.png"});
+  await page.locator(".div-half .div-half a").first().click();
+  let content = await page.locator("pre").textContent();
+
+  let contentArray = content.split("...");
+  let trimmedArray = contentArray.slice(1);
+  let mappedArray = 
+    trimmedArray.map(element => element.replace(/(\r\n|\n|\r)/gm, " ").trim())
+    .map(element => element.replace("  ", " "));
+  console.log(mappedArray.toString());
+  await browser.close();
+})();
